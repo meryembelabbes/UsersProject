f99877f073cb63da403167c78d17428a9d6b68c1
Blondint <Blondin.thibaud@gmail.com>
2016-06-01 12:38:15 +0200

    user ontroller

--- edu.telecom.lille.projet.info.etudiantUser/src/userController/UserController.java
+++ edu.telecom.lille.projet.info.etudiantUser/src/userController/UserController.java
@@ -1,34 +1,153 @@
-package userModel;
+package userController;
 
-import userController.UserController;
+import userModel.UserDB;
 /**
- * Cette classe permet de tester les fonctions du contrÃ´leur d'utilisateurs.
- * Elle crÃ©e une base de donnÃ©es de 6 utilisateurs et les sauvegarde dans le fichier "usersDB.xml". 
+ * Cette classe est le contrôleur d'utilisateurs que vous devez implémenter. 
+ * Elle contient un attribut correspondant à la base de données utilisateurs que vous allez créer.
+ * Elle contient toutes les fonctions de l'interface IUserController que vous devez implémenter.
  * 
- * @author Jose Mennesson (Mettre Ã  jour)
- * @version 04/2016 (Mettre Ã  jour)
+ * @author Jose Mennesson (Mettre à jour)
+ * @version 04/2016 (Mettre à jour)
  * 
  */
 
-//TODO Classe pouvant Ãªtre modifiÃ©e suivant vos besoins
+//TODO Classe à modifier
 
-public class Main {
+public class UserController implements IUserController
+{
+	
 	/**
-	 * Fonction principale 
-	 * 
-	 * @param args
-	 * 			Les arguments donnÃ©s en entrÃ©e du programme.
+	 * Contient une instance de base de données d'utilisateurs
 	 * 
 	 */
-	public static void main(String[] args) {
-		final String file="usersDB.xml";
-		UserController UC=new UserController(file);
-		UC.addAdmin("su","KF",0001,"Kevin", "Flynn",  "@tron");
-		UC.addAdmin("su","KR",0002,"Keanu", "Reeves",  "redpill");
-		UC.addTeacher("su","GS",1001,"Grand", "Schtroumpf",  "salsepareille");
-		UC.addTeacher("su","MF",1002,"Morgan", "Freeman",  "iknowall");
-		UC.addStudent("su","BS",2001,"Buffy", "Summers",  "stake");
-		UC.addStudent("su","NL",2002,"Nicolas", "Lepetit",  "prout");
-		UC.saveDB();
+	private UserDB userDB=null;
+	
+	
+	/**
+	 * Constructeur de controleur d'utilisateurs créant la base de données d'utilisateurs
+	 * 
+	 * @param userfile
+	 * 		Fichier XML contenant la base de données d'utilisateurs
+	 */
+	public UserController(String userfile){
+		UserDB userDB=new UserDB(userfile);
+		this.setUserDB(userDB);
 	}
+
+	@Override
+	public String getUserName(String userLogin) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getUserClass(String userLogin, String userPwd) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getStudentGroup(String studentLogin) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean addAdmin(String adminLogin, String newAdminlogin, int adminID, String firstname, String surname,
+			String pwd) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addTeacher(String adminLogin, String newteacherLogin, int teacherID, String firstname,
+			String surname, String pwd) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addStudent(String adminLogin, String newStudentLogin, int studentID, String firstname,
+			String surname, String pwd) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeUser(String adminLogin, String userLogin) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addGroup(String adminLogin, int groupId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeGroup(String adminLogin, int groupId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean associateStudToGroup(String adminLogin, String studentLogin, int groupId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String[] usersToString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String[] usersLoginToString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String[] studentsLoginToString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String[] groupsIdToString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String[] groupsToString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean loadDB() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean saveDB() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public UserDB getUserDB() {
+		return userDB;
+	}
+
+	public void setUserDB(UserDB userDB) {
+		this.userDB = userDB;
+	}
+	
+	
+
 }
+
